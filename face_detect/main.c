752981ad45018d0e6d6fbf3ee01dbd55868e43b7
zhaozhongxiang <zhaozhongxiang@canaan-creative.com>
2018-11-19 19:53:57 +0800

    Face_detect and licheedan

--- face_detect/main.c
+++ face_detect/main.c
@@ -7,6 +7,7 @@
 #include "fpioa.h"
 #include "lcd.h"
 #include "ov5640.h"
+#include "ov2640.h"
 #include "plic.h"
 #include "sysctl.h"
 #include "uarths.h"
@@ -14,7 +15,7 @@
 #include "utils.h"
 #include "kpu.h"
 #include "region_layer.h"
-
+#include "board_config.h"
 
 #define CLASS_NUMBER     (1)
 
@@ -66,6 +67,24 @@ static int on_irq_dvp(void* ctx)
 
 static void io_mux_init(void)
 {
+#if BOARD_LICHEEDAN
+    /* Init DVP IO map and function settings */
+    fpioa_set_function(42, FUNC_CMOS_RST);
+    fpioa_set_function(44, FUNC_CMOS_PWDN);
+    fpioa_set_function(46, FUNC_CMOS_XCLK);
+    fpioa_set_function(43, FUNC_CMOS_VSYNC);
+    fpioa_set_function(45, FUNC_CMOS_HREF);
+    fpioa_set_function(47, FUNC_CMOS_PCLK);
+    fpioa_set_function(41, FUNC_SCCB_SCLK);
+    fpioa_set_function(40, FUNC_SCCB_SDA);
+
+    /* Init SPI IO map and function settings */
+    fpioa_set_function(38, FUNC_GPIOHS0 + DCX_GPIONUM);
+    fpioa_set_function(36, FUNC_SPI0_SS3);
+    fpioa_set_function(39, FUNC_SPI0_SCLK);
+
+    sysctl_set_spi0_dvp_data(1);
+#else
     /* Init DVP IO map and function settings */
     fpioa_set_function(11, FUNC_CMOS_RST);
     fpioa_set_function(13, FUNC_CMOS_PWDN);
@@ -82,13 +101,22 @@ static void io_mux_init(void)
     fpioa_set_function(7, FUNC_SPI0_SCLK);
 
     sysctl_set_spi0_dvp_data(1);
+#endif
 }
 
 static void io_set_power(void)
 {
-    /* Set dvp and spi pin to 1.8V */
-    sysctl_set_power_mode(SYSCTL_POWER_BANK1, SYSCTL_POWER_V18);
-    sysctl_set_power_mode(SYSCTL_POWER_BANK2, SYSCTL_POWER_V18);
+#if BOARD_LICHEEDAN
+        /* Set dvp and spi pin to 1.8V */
+        sysctl_set_power_mode(SYSCTL_POWER_BANK6, SYSCTL_POWER_V18);
+        sysctl_set_power_mode(SYSCTL_POWER_BANK7, SYSCTL_POWER_V18);
+
+#else
+        /* Set dvp and spi pin to 1.8V */
+        sysctl_set_power_mode(SYSCTL_POWER_BANK1, SYSCTL_POWER_V18);
+        sysctl_set_power_mode(SYSCTL_POWER_BANK2, SYSCTL_POWER_V18);
+#endif
+
 }
 
 
@@ -184,13 +212,26 @@ int main(void)
     /* LCD init */
     printf("LCD init\n");
     lcd_init();
+#if BOARD_LICHEEDAN
+    #if OV5640
+        lcd_set_direction(DIR_YX_RLUD);
+    #else
+        lcd_set_direction(DIR_YX_RLDU);
+    #endif
+#else
+    #if OV5640
+        lcd_set_direction(DIR_YX_LRUD);
+    #else
+        lcd_set_direction(DIR_YX_LRDU);
+    #endif
+#endif
     lcd_clear(BLACK);
     lcd_draw_string(136, 70, "DEMO 1", WHITE);
     lcd_draw_string(104, 150, "face detection", WHITE);
 
     /* DVP init */
     printf("DVP init\n");
-
+    #if OV5640
     dvp_init(16);
     dvp_enable_burst();
     dvp_set_output_enable(0, 1);
@@ -198,7 +239,16 @@ int main(void)
     dvp_set_image_format(DVP_CFG_RGB_FORMAT);
     dvp_set_image_size(320, 240);
     ov5640_init();
-
+    #else
+    dvp_init(8);
+    dvp_set_xclk_rate(24000000);
+    dvp_enable_burst();
+    dvp_set_output_enable(0, 1);
+    dvp_set_output_enable(1, 1);
+    dvp_set_image_format(DVP_CFG_RGB_FORMAT);
+    dvp_set_image_size(320, 240);
+    ov2640_init();
+    #endif
     dvp_set_ai_addr((uint32_t)g_ai_buf, (uint32_t)(g_ai_buf + 320 * 240), (uint32_t)(g_ai_buf + 320 * 240 * 2));
     dvp_set_display_addr((uint32_t)g_lcd_gram0);
     dvp_config_interrupt(DVP_CFG_START_INT_ENABLE | DVP_CFG_FINISH_INT_ENABLE, 0);
