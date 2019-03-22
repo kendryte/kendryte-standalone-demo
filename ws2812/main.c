6789447afebacb2a58103e5acf9a79435c49295a
zhaozhongxiang <zhaozhongxiang@canaan-creative.com>
2019-03-22 21:26:01 +0800

    Add ws2812 driver by i2s

--- ws2812/main.c
+++ ws2812/main.c
@@ -6,10 +6,11 @@
 #include <uarths.h>
 #include "ws2812.h"
 
-#define WS_GPIOHS_TEST 1
+#define WS_GPIOHS_TEST 0
 #define WS_SPI_TEST 0
+#define WS_I2S_TEST 1
 
-#if WS_GPIOHS_TEST + WS_SPI_TEST != 1
+#if WS_GPIOHS_TEST + WS_SPI_TEST + WS_I2S_TEST!= 1
 #error set WS_GPIOHS_TEST or WS_SPI_TEST
 #endif
 
@@ -27,11 +28,17 @@ int main(void)
     uarths_init();
 
     #if WS_SPI_TEST
-    ws2812_init_spi(WS_PIN);
+    ws2812_init_spi(WS_PIN, 2);
     #endif
 
     #if WS_GPIOHS_TEST
     ws2812_init_gpiohs(WS_PIN, WS_GPIOHS_NUM);
+    #endif
+
+    #if WS_I2S_TEST
+    #define I2S_NUM I2S_DEVICE_1
+    sysctl_pll_set_freq(SYSCTL_PLL2, 45158400UL);
+    ws2812_init_i2s(WS_PIN, I2S_NUM, 3, 350000);
     #endif
 
     ws_info = ws2812_get_buf(6);
@@ -50,6 +57,20 @@ int main(void)
             sleep(1);
             ws2812_set_data(ws_info, 0, 0xFF, 0xFF, 0);
             ws2812_send_data_spi(0, 1, ws_info);
+            sleep(1);
+        #endif
+        #if WS_I2S_TEST
+            ws2812_set_data(ws_info, 0, 0xFF, 0, 0);
+            ws2812_send_data_i2s(I2S_NUM, 1, ws_info);
+            sleep(1);
+            ws2812_set_data(ws_info, 0, 0, 0xFF, 0);
+            ws2812_send_data_i2s(I2S_NUM, 1, ws_info);
+            sleep(1);
+            ws2812_set_data(ws_info, 0, 0, 0, 0xFF);
+            ws2812_send_data_i2s(I2S_NUM, 1, ws_info);
+            sleep(1);
+            ws2812_set_data(ws_info, 0, 0xFF, 0xFF, 0);
+            ws2812_send_data_i2s(I2S_NUM, 1, ws_info);
             sleep(1);
         #endif
         #if WS_GPIOHS_TEST
